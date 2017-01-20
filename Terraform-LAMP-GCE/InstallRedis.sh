ae16b29250f0a4069890410d211a0dcc630524b7
ashwin-ashok-93 <ashwin.ashok@applayer.com>
2017-01-20 08:05:02 +0530

    Add files via upload

--- Terraform-LAMP-GCE/InstallRedis.sh
+++ Terraform-LAMP-GCE/InstallRedis.sh
@@ -4,7 +4,8 @@ replace () {
    file=$1
    var=$2
    new_value=$3
-   awk -v var="$var" -v new_val="$new_value" 'BEGIN{FS=OFS="="}match($1, "^\\s*" var "\\s*") {$2=" " new_val}1' "$file"
+   #awk -v var="$var" -v new_val="$new_value" 'BEGIN{FS=OFS=" "}match($1, "^\\s*" var "\\s*") {$2=" " new_val}1' $1
+   sed -i "s/\($2 * *\).*/\1$3/" $1
 }
 
 sudo apt-get -y update
@@ -24,30 +25,39 @@ cd /tmp/redis
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH="/tmp/redis/redis-stable"
 
-REDIS_PORT=6379
-#REDIS_PORT=$1
-REDIS_CONFIG_FILE="/etc/redis/$REDIS_PORT.conf"
-REDIS_DATA_DIR="/var/lib/redis/$REDIS_PORT"
-REDIS_LOG_FILE="/var/log/redis_$REDIS_PORT.log"
+#REDIS_PORT=6379
+REDIS_PORT=$1
+
+echo ${REDIS_PORT}
+
+REDIS_CONFIG_FILE="/etc/redis/${REDIS_PORT}.conf"
+REDIS_DATA_DIR="/var/lib/redis/${REDIS_PORT}"
+REDIS_LOG_FILE="/var/log/redis_${REDIS_PORT}.log"
 REDIS_EXECUTABLE=`command -v redis-server`
 CLI_EXEC=`command -v redis-cli`
 INIT_SCRIPT_DEST="/etc/init.d/redis_${REDIS_PORT}"
 
-mkdir -p `dirname "$REDIS_CONFIG_FILE"`
-mkdir -p `dirname "$REDIS_LOG_FILE"`
-mkdir -p "$REDIS_DATA_DIR"
+mkdir -p `dirname "${REDIS_CONFIG_FILE}"`
+mkdir -p `dirname "${REDIS_LOG_FILE}"`
+mkdir -p "${REDIS_DATA_DIR}"
 
 cd /tmp/redis/redis-stable
 cp redis.conf ${REDIS_CONFIG_FILE}
+
 replace ${REDIS_CONFIG_FILE} "port" ${REDIS_PORT}
 replace ${REDIS_CONFIG_FILE} "bind" "0.0.0.0"
+replace ${REDIS_CONFIG_FILE} "daemonize" "yes"
+replace ${REDIS_CONFIG_FILE} "supervised" "auto"
 
 cd /tmp/redis/redis-stable/utils
 cp redis_init_script ${INIT_SCRIPT_DEST}
+
 replace ${INIT_SCRIPT_DEST} "REDISPORT" ${REDIS_PORT}
 
 sudo service redis_${REDIS_PORT} start &
-exit 0
+redis-server /etc/redis/${REDIS_PORT}.conf &
+redis-server /etc/redis/${REDIS_PORT}.conf 
 
 
+echo "Started the Server"
 
--- Terraform-LAMP-GCE/main.tf
+++ Terraform-LAMP-GCE/main.tf
@@ -1,6 +1,5 @@
 # See https://cloud.google.com/compute/docs/load-balancing/network/example
 
-
 variable "install_script_src_path" {
   description = "Path to install script within this repository"
   default     = "/data/Terraform-LAMP-GCE/InstallRedis.sh"
@@ -23,7 +22,7 @@ variable "client_script_dest_path" {
 
 variable "redis_server_port" {
   description = "Path to put the install script on each destination resource"
-  default     = 6379
+  default     = 7000
 }
 
 
@@ -38,7 +37,27 @@ provider "google" {
   credentials = "${file("credentials.json")}"
 }
 
+resource "google_compute_http_health_check" "default" {
+  name                = "tf-redis-basic-check"
+  request_path        = "/"
+  check_interval_sec  = 1
+  healthy_threshold   = 1
+  unhealthy_threshold = 10
+  timeout_sec         = 1
+}
 
+resource "google_compute_target_pool" "default" {
+  name          = "tf-redis-target-pool"
+  instances     = ["${google_compute_instance.redis-server.*.self_link}",
+                   "${google_compute_instance.client.*.self_link}"]
+  health_checks = ["${google_compute_http_health_check.default.name}"]
+}
+
+resource "google_compute_forwarding_rule" "default" {
+  name       = "tf-redis-forwarding-rule"
+  target     = "${google_compute_target_pool.default.self_link}"
+  port_range = "80"
+}
 
 
 resource "google_compute_instance" "redis-server" {
@@ -68,10 +87,7 @@ resource "google_compute_instance" "redis-server" {
       agent       = false
     }
       source      = "${var.install_script_src_path}"
-    destination = "${var.install_script_dest_path}"
-
- 
-
+      destination = "${var.install_script_dest_path}"
   }
 
   provisioner "remote-exec" {
@@ -82,7 +98,9 @@ resource "google_compute_instance" "redis-server" {
       agent       = false
     }
    
-     inline = ["chmod +x ${var.install_script_dest_path} && ${var.install_script_dest_path} "]
+     inline = ["chmod +x ${var.install_script_dest_path}",
+               "${var.install_script_dest_path} ${var.redis_server_port}",
+               " redis-server /etc/redis/${var.redis_server_port}.conf"]
   }
 
 
@@ -96,6 +114,8 @@ resource "google_compute_instance" "redis-server" {
     scopes = ["https://www.googleapis.com/auth/compute.readonly"]
   }
 }
+
+#--------------------------------------------------------------#
 
 resource "google_compute_instance" "client" {
   name         = "tf-client"
@@ -137,9 +157,13 @@ resource "google_compute_instance" "client" {
       agent       = false
     }
    
-     inline = ["chmod +x ${var.client_script_dest_path}",
-                "python2 ${var.client_script_dest_path} ${google_compute_instance.redis-server.network_interface.0.address} ${var.redis_server_port}"]
+     inline = ["sudo apt-get -y update",
+                "sudo apt-get -y install python-redis",
+                "touch ${var.client_script_dest_path}/log.txt",
+                "python2 ${var.client_script_dest_path} ${google_compute_instance.redis-server.network_interface.0.address} ${var.redis_server_port}"
+                ]
   }
+  
   metadata {
     ssh-keys = "root:${file("/home/ashwin/.ssh/modables-demo-bucket.pub")}"
   }
